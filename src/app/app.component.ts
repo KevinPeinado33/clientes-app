d10f9e117d332505ec66d3adc334ef0b10f698e1
KevinPeinado <kevinpeinado@upeu.edu.pe>
2020-11-23 22:20:25 -0500

    iniciando el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'clientes-app';
+
+  curso: string = 'Spring boot y angular 10+';
+  alumno: string = 'kevin peinado leiva';
+
 }
