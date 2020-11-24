4dab38ee85cb5da987642897666105681fc1b7a7
KevinPeinado <kevinpeinado@upeu.edu.pe>
2020-11-24 17:07:42 -0500

    directivas, obervables, capa service

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,11 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+    {id: 1,nombre: 'Dayana', apellido: 'Peinado', createAt: '2020-10-10', email: 'dayana@gmail.com'},
+    {id: 2,nombre: 'Kevin', apellido: 'Leiva', createAt: '2020-12-10', email: 'kevin@gmail.com'},
+    {id: 3,nombre: 'Leo', apellido: 'Martinez', createAt: '2020-03-10', email: 'Leo@gmail.com'},
+    {id: 4,nombre: 'Kaori', apellido: 'Solorzola', createAt: '2020-11-10', email: 'kaori@gmail.com'},
+    {id: 5,nombre: 'Sofia', apellido: 'Perez', createAt: '2020-01-10', email: 'sofia@gmail.com'},
+    {id: 6,nombre: 'Caeli', apellido: 'Guitierrez', createAt: '2020-02-10', email: 'caeli@gmail.com'},
+    {id: 7,nombre: 'Adrian', apellido: 'Flores', createAt: '2020-03-10', email: 'adrian@gmail.com'},
+  ];
