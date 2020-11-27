d11e82198f2ca47246815a293e907215ee59e866
KevinPeinado <kevinpeinado@upeu.edu.pe>
2020-11-26 22:45:44 -0500

    crud terminado, usando sweetalert2

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,22 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
 })
 
-export class ClientesComponent implements OnInit{
+export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
 
-  constructor( private clienteService: ClienteService  ) { }
+  constructor(private clienteService: ClienteService) { }
 
   ngOnInit() {
     this.clienteService.getClientes().subscribe(
       clientes => this.clientes = clientes
     );
+  }
+
+  delete(cliente: Cliente): void {
+    Swal.fire({
+      title: 'Estas seguro?',
+      text: `¿Seguro que desea eliminar al cliente ${cliente.nombre}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar!',
+      cancelButtonText: 'No, cancelar!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+          this.clienteService.delete(cliente.id).subscribe( //el metodo suscribe() esucha todo
+            response => {
+              this.clientes = this.clientes.filter(cli => cli !== cliente) // va a filtrar el que sea igual al que viene capturado del boton
+              Swal.fire(
+                'Cliente Eliminado!',
+                `${cliente.nombre} ha sido eliminado con éxito.`,
+                'success'
+              )
+            }
+          )
+      }
+    })
   }
 
 }
